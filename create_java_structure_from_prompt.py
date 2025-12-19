5aac882add1f8d4bb3013a99b7cd2528536bff03
Shefan756 <shefantharani42@gmail.com>
2025-12-19 11:14:18 +0530

    deleted unnecessary files

--- create_java_structure_from_prompt.py
+++
@@ -1,8 +0,0 @@
-from omni_automator.core.ai_task_executor import AITaskExecutor
-
-prompt = "create a folder called java on Desktop and in tha fodler create 15 fodlers called java_1 and so on and in each of those java_1 to java_10 create folders called src and module and config module and also a text document along with those src and config and modules folder which contain text hi i am java_1 adn so on upto java_10 folders"
-
-if __name__ == '__main__':
-    executor = AITaskExecutor()
-    res = executor.parse_and_execute_nl(prompt)
-    print(res)
